2f095019e44d3e74c247c29539602a59738bb7f5
Kai Chang <kaijchang@gmail.com>
2020-10-01 18:21:29 -0700

    basic driving code

--- robot.py
+++ robot.py
@@ -1,9 +1,18 @@
 import wpilib
-import wpilib.drive
 
 class MyRobot(wpilib.TimedRobot):
   def robotInit(self):
-    pass
+    self.left_drive_motors = wpilib.VictorSP(0)
+    self.right_drive_motors = wpilib.VictorSP(1)
+
+    self.left_drive_motors.setInverted(True)
+
+    self.left_joystick = wpilib.Joystick(0)
+    self.right_joystick = wpilib.Joystick(1)
+
+  def teleopPeriodic(self):
+    self.left_drive_motors.set(self.left_joystick.getY())
+    self.right_drive_motors.set(self.right_joystick.getY())
 
 if __name__ == '__main__':
   wpilib.run(MyRobot)
