3b5c00d2c5378fb11676da0082d60e8ae508f11e
qivery <humphreyylu>
2020-12-04 21:30:32 -0800

    code

--- robot.py
+++ robot.py
@@ -4,8 +4,8 @@ class MyRobot(wpilib.TimedRobot):
   def robotInit(self):
     self.left_drive_motors = wpilib.VictorSP(0)
     self.right_drive_motors = wpilib.VictorSP(1)
-    self.right_drive_motors.setInverted(True)
-
+    self.arm_drive_motors = wpilib.VictorSP(2)
+    
     self.left_joystick = wpilib.Joystick(0)
     self.right_joystick = wpilib.Joystick(1)
 
@@ -13,12 +13,16 @@ class MyRobot(wpilib.TimedRobot):
     self.timer = wpilib.Timer()
     self.timer.start()
   def autonomousPeriodic(self):
-    if self.timer.get() < 1:
-      self.left_drive_motors.set(1)
-      self.right_drive_motors.set(1)
+    if self.timer.get() < 2.2:
+      self.left_drive_motors.set(-0.3)
+      self.right_drive_motors.set(-0.4)
     else:
       self.left_drive_motors.set(0)
       self.right_drive_motors.set(0)
+    if self.timer.get() < 1:
+      self.arm_drive_motors.set(-0.1)
+    else:
+      self.arm_drive_motors.set(0)
 
 
 
@@ -27,5 +31,14 @@ class MyRobot(wpilib.TimedRobot):
     self.left_drive_motors.set(self.left_joystick.getY())
     self.right_drive_motors.set(self.right_joystick.getY())
 
+    if self.left_joystick.getRawButton(1):
+      self.arm_drive_motors.set(0.5)
+      pass
+    elif self.left_joystick.getRawButton(2):
+      self.arm_drive_motors.set(0)
+      pass
+    if self.left_joystick.getRawButton(3):
+      self.arm_drive_motors.set(-0.5)
+      pass
 if __name__ == '__main__':
   wpilib.run(MyRobot)
